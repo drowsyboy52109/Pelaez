aef1bee1d01692a2668141ab3d761fc669daa57e
drowsyboy52109 <147647987+drowsyboy52109@users.noreply.github.com>
2025-09-30 15:59:38 -0500

    Add files via upload
    Robot.java plus library

--- Robot.java
+++ Robot.java
@@ -5,7 +5,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
  * the TimedRobot documentation. If you change the name of this class or the package after creating
@@ -16,6 +20,9 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
+  XboxController xb1=new XboxController(0);
+  SparkMax motor1=new SparkMax(3, MotorType.kBrushless);
+  
   public Robot() {}
 
   @Override
@@ -31,8 +38,11 @@ public class Robot extends TimedRobot {
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
-
+  public void teleopPeriodic() {
+    double speed = xb1.getRightTriggerAxis();
+    motor1.set(speed*.1);
+    SmartDashboard.putNumber("Right trigger", speed);
+  }
   @Override
   public void disabledInit() {}
 
